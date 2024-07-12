0580b7bb2ba23ce59cad71319583dacf6d7d023c
marcelo.rodrigues <marcelo.rodrigues@metasix.com.br>
2024-07-12 15:28:24 -0300

    adicionando todos os arquivos

---
+++ src/main/java/med/voll/api/controller/MedController.java
@@ -0,0 +1,20 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import med.voll.api.entidade.DadosCadastroMedico;
+
+
+@RestController
+@RequestMapping("cadastroMedicos")
+public class MedController {
+
+    @PostMapping
+    public DadosCadastroMedico cadastrar(@RequestBody DadosCadastroMedico dados){
+        return dados;
+    }
+
+}
---
+++ src/main/java/med/voll/api/enumMedic/Especialidade.java
@@ -0,0 +1,8 @@
+package med.voll.api.enumMedic;
+
+public enum Especialidade {
+    ORTOPEDIA, 
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA
+}
